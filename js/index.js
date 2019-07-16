677a79ce5e7cba9147050d349ea60b34d4e69da4
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-07-16 19:43:42 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// use this to copy code snippets or use your browser's console
+function justInvoke(fn) { return fn() }
+
+function setThisWithCall(fn, thisValue, arg) { return fn.call(thisValue, arg) }
+
+function setThisWithApply(fn, thisValue, args) { return fn.apply(thisValue, args) }
+
+function returnNewFunctionOf(fn, thisValue) { return fn.bind(thisValue) }
